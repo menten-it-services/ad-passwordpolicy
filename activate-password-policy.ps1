aabf022b2779c25d1113218bab068f749d03575a
Konrad Schmitz <konrad.schmitz@menten.com>
2021-03-22 17:39:40 +0100

    Deleted double line breaks and replaced by simple ones

--- activate-password-policy.ps1
+++ activate-password-policy.ps1
@@ -2,15 +2,11 @@
 The script needs to be executed as a domain administrator
 #>
 
-
 #  Latest Powershell found at:    https://github.com/PowerShell/PowerShell/releases/tag/v7.1.3
 #  Powershell Modules found at:   https://www.microsoft.com/en-us/download/details.aspx?id=45520
-
 #  Using PSRemoting for using Powershell Module installed on Domaincontroller (Enable the next lines if you do not have the module installed)
-
 #  FQDN of Domaincontroller
 $domainController = 'Your Domain Controller'
-
 
 #  Checks for RemoteAD Powershellmodule and installs it if neccesarry
 if(Get-Module -ListAvailable -Name RemoteAD){
@@ -23,17 +19,13 @@ else{
     Import-Module RemoteAD
 }
 
-
 #  Define if the script should make the changes or just run in debug
 $debug = 0
-
 #  Scriptpath 
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-
 # Define the location for the log file and clear the content of the file
 $logfile = $scriptPath+"\log\user_passwordchange_activation.log"
 Clear-Content $logfile
-
 
 # Function for writing information inside a user_passwordchange_activation.log with current date and time
 Function LogWrite
@@ -42,7 +34,6 @@ Function LogWrite
    Write-Host  "$(get-date -format "yyyy-mm-dd H:mm:ss"): $logstring"
    Add-content $Logfile -value "$(get-date -format "yyyy-mm-dd H:mm:ss"): $logstring"
 }
-
 
 <# 
 Contains name, firstname, email, change, salutation and department (for orientation purposes) formatted as a table seperated with "," for every userobject
@@ -53,7 +44,6 @@ IT-Department,Misses,Ana,Admin,yes,admins@menten.com
 # Define the location for the csv file used to import the needed information for each userobject and wether the userobject's password should be changed ("yes" if yes)
 $csvFile = $scriptPath+"\users.csv" 
 
-
 # Imports the information from the csv and executes for each userobject
 Import-Csv $csvFile | ForEach-Object {
     
@@ -62,21 +52,17 @@ Import-Csv $csvFile | ForEach-Object {
    $name = ($_.Name)
    $change = ($_.Change)
 
-
    # Checks if email for current userobject is available
    if($email -eq ""){
         return
    }
 
-
    # Build samaccountname from userobject's attribute emailadress
    $samaccountname = (Get-ADUser -filter * -Properties * | where emailaddress -like $email).samaccountname
-
 
     # Checks wether to change the options or not
     if($change -eq "yes"){
         try{
-
 
             # Deactivates Password never expires 
             Logwrite "Change user login option in active directory for $samaccountname"
@@ -85,13 +71,10 @@ Import-Csv $csvFile | ForEach-Object {
             # Writes changes for current userobject in log file
             Logwrite "Uncheck checkbox ""Password never expires"" for Account: $samaccountname"
 
-
             # Sets Change password at next login
             Logwrite "Force $samaccountname user to change password with checkbox ""Change pasword at next logon"""
-
             #  Make the Change that user needs to change password at next logon
             if(!$debug){Set-ADUser -Identity $samaccountname -ChangePasswordAtLogon $true}
-
         }catch{
             # Logs error while changing the options
             LogWrite "Error while changing the options: $samaccountname"
--- sending-mail-for-information.ps1
+++ sending-mail-for-information.ps1
@@ -2,17 +2,13 @@
 The script needs to be executed as a domain administrator
 Change unicode for your language if needed (replace "UTF8")
 #>
-
 #  Define if the script should make the changes or just run in debug
 $debug = 0
-
 #  Scriptpath 
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-
 #  Define the location for the log file and clear the content of the file
 $logfile = $scriptPath+"\log\email_sent_to.log"
 Clear-Content $logfile
-
 #  Function for writing information inside a email_sent_to.log with current date and time
 Function LogWrite
 {
@@ -21,14 +17,12 @@ Function LogWrite
    Add-content $Logfile -value "$(get-date -format "yyyy-mm-dd H:mm:ss"): $logstring"
 }
 
-
 #  Define the subject for the informative email, the originator (and bcc if wanted)
 $changeDate = (Get-Date).AddDays(7).ToString('yyyy-MM-dd')
 $subject = "Upcoming change of passwords on $changeDate"
 $originatorEmail = "Administrator <admins@menten.com>"
 $bcc = "admins@menten.com"
 $smtpserver= "sophos.menten.com"  
-
 
 <#  
 Contains name, first_name, email, change, salutation and department (for orientation purposes) formatted as a table seperated with "," for every userobject
@@ -39,7 +33,6 @@ IT-Department,Misses,Ana,Admin,yes,admins@menten.com
 #  Define the location for the csv file used to import the needed information for each userobject and wether the userobject's password should be changed ("yes" if yes)
 $csvFile = $scriptPath+"\users.csv"
 
-
 #  Imports the information from the csv and executes for each userobject
 Import-Csv $csvFile | ForEach-Object {
   
@@ -48,30 +41,25 @@ Import-Csv $csvFile | ForEach-Object {
    $name = ($_.name)
    $change = ($_.change)
    
-
    #  Defines the location for the mailbody (and attachment if wanted) u want to use
    $body = $scriptPath+"\mailbody.html"
    $body = Get-Content $body -Encoding UTF8 | Out-String
    $attachment = $scriptPath+"\attachment.pdf"
-
 
    #  Replaces the variables inside mailbody.html with name and salutation for each userobject for each run
    $body = $body.replace("%salutation%",$salutation)
    $body = $body.replace("%name%",$name)
    $body = $body.replace("%currentDate+7%",$changeDate)
 
-
    try {
      #  Checks wether to send an email to the current userobject 
      if($change -eq "yes") {
-
 
         #  Writes information in the log
         Logwrite "Use the following information: $salutation, $name"
         LogWrite "Send mail to: $email"
         LogWrite "Send-MailMessage -To $email -from $originatorEmail -bcc $bcc -Subject $subject -Body $body -BodyAsHtml -encoding ([System.Text.Encoding]::UTF8) -Attachments $attachment -SmtpServer exchange.koeln.egetuerk.de"
         
-
         #  Sends the email to current userobject
         if(!$debug){Send-MailMessage -To $email -from $originatorEmail -bcc $bcc -Subject $subject -Body $body -BodyAsHtml -encoding ([System.Text.Encoding]::UTF8) -Attachments $attachment -SmtpServer $smtpServer}
      }
@@ -81,4 +69,3 @@ Import-Csv $csvFile | ForEach-Object {
    } 
    
 }
-
